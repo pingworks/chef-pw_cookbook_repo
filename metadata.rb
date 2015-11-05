9b50edcb48c894a7482b797b515923f94fee84bd
Christoph Lukas <lukas@pingworks.de>
2015-11-05 14:51:19 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'christoph.lukas@gmx.net'
 license          'Apache 2.0'
 description      'Installs/Configures pw_cookbook_repo'
 long_description 'Installs/Configures pw_cookbook_repo'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'pw_testhelper'
 depends 'apt'
