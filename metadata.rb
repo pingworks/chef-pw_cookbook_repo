1e0b2e1c20fa0f463c46bb003e9e853cfe4b4664
Alexander Birk <birk@pingworks.de>
2016-05-04 01:29:37 +0200

    Release v0.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'christoph.lukas@gmx.net'
 license          'Apache 2.0'
 description      'Installs/Configures pw_cookbook_repo'
 long_description 'Installs/Configures pw_cookbook_repo'
-version          '0.2.1'
+version          '0.2.2'
 
 depends 'pw_testhelper'
 depends 'apt'
